21f3f136399ef4abbfbdf4b9ac15c4c9984c138d
Sebaabes1980 <110298039+Sebaabes1980@users.noreply.github.com>
2023-08-10 12:34:32 -0300

    1

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -9,42 +7,28 @@ function App() {
   return (
     <>
       <header>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+        <a href="/index.html" target="_blank">
+          <img src="/src/assets/usuario.png" alt="logo de usuario" />
         </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1>My Tinerary</h1>
         <nav>
-        <ul class="navbar-nav">
-          <li class="nav-item">
-            <a class="nav-link" href="/index.html">Inicio</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="/upcoming.html">Acceso Autorizado</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="/past.html">Novedades de la DNRPA</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="/contact.html">Formulario de contacto</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="/stats.html">Radicación por dominio</a>
-          </li>
-        </ul>
-      </nav>
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link" href="/index.html">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/stats.html">Cities</a>
+            </li>
+          </ul>
+        </nav>
       </header>
       <main>
-        <h1>Vite + React</h1>
-        <div className="card">
-          <button onClick={() => setCount((count) => count + 1)}>
-            count is {count}
-          </button>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test HMR
-          </p>
-        </div>
+        <article>
+          <h1>Find the perfect destination</h1>
+          <h5>Our app will help you find the perfect path for your next trip. With an easy-to-use interface and a host of itinerary options, planning your next trip has never been easier.</h5>
+          <button href="/index.html"> View More  </button>
+        </article>
+        <img src="/src/assets/BsAs.jpg" alt='foto de Buenos Aires'/>
       </main> 
       <footer> 
       <div id="contenedor-redes">
@@ -52,7 +36,7 @@ function App() {
         <a href="https://web.facebook.com/mytinerary"> <img class="imgFooter" src="/src/assets/facebook.png" alt="logo de facebook"/> </a>
         <a href="https://web.whatsapp.com/"> <img class="imgFooter" src="/src/assets/whatsapp.png" alt="logo de whatsapp"/> </a>
       </div>
-      <h1>INSTITUTO DE ESTUDIOS SUPERIORES ARGENTINO</h1>
+      <h1>Powered by Minhub</h1>
       </footer>
     </>
   )
